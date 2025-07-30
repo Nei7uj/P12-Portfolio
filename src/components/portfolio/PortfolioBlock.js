d1c2aa12557462534eebffb6b23e4930b9b2ca0c
nei7uj <julien.dille@gmail.com>
2025-07-30 10:19:21 +0200

    Ajouts Code Source

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -14,7 +14,7 @@ function PortfolioBlock(props) {
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
+               <IconLink link={source} title={'Code Source'} icon={'fa fa-code'}/>
             </Box>
          </Box>
       </Box>
